c99d5df743d5251ea4503357197f16e549a1cc42
Alvaro Lupercio <lupercio0184@outlook.com>
2019-03-02 23:14:49 -0500

    Begun using git hub again. Initial commit after resyncing repo.

--- Modules/PSTools/PSTools.psm1
+++ Modules/PSTools/PSTools.psm1
@@ -1,5 +1,5 @@
-$ErrorLogPreference = "C:\retry.txt"
-$ConnectionString = "server=sql-csv;database=inventory;trusted_connection=true"
+$ErrorLogPreference = "D:\FunctionLogs\ErrorLog.txt"
+$ConnectionString = "server=lco-sql1;database=inventory;user id=sa;password=B33b0p@!"
 
 Import-Module PSDatabase
 
@@ -13,8 +13,8 @@ function Get-ComputerNamesFromDatabase {
     [CmdletBinding()]
 
     param(
-        [Parameter(Mandatory = $true)]
-        [string]$Query
+        [Parameter()]
+        [string]$Query = "SELECT computername FROM lco_servers"
     )
 
     Get-DatabaseData -ConnectionString $ConnectionString -mssql -query $Query
@@ -38,7 +38,7 @@ function Set-InventoryInDatabase {
 
     PROCESS {
         foreach ($obj in $InputObject) {
-            $query = "UPDATE computers_computer SET
+            $query = "UPDATE lco_servers SET
                        biosserial = '$($obj.BIOSSerial)',
                        manufacturer = '$($obj.Manufacturer)',
                        model = '$($obj.Model)',
@@ -94,7 +94,7 @@ Function Get-SystemData {
     [CmdletBinding()]
         
     param(
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [Alias("Hostname")]
         [string[]]$ComputerName,
         [string]$ErrorLog,
@@ -110,11 +110,12 @@ Function Get-SystemData {
         
         foreach ($computer in $ComputerName) {
             Write-Verbose "Querying $computer"
+            $cim = New-CimSession -ComputerName $computer -Credential $admin
         
             # Test for online systems
             try {
                 $everything_ok = $true
-                $os = Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName $computer -ErrorAction Stop
+                $os = Get-CimInstance -ClassName Win32_OperatingSystem -CimSession $cim -ErrorAction Stop
             }
             catch {
                 if ($LogErrors) {
@@ -127,8 +128,8 @@ Function Get-SystemData {
         
             # Systems online. Continue processing
             if ($everything_ok) {
-                $cs = Get-CimInstance -ClassName Win32_ComputerSystem -ComputerName $computer
-                $bios = Get-CimInstance -ClassName Win32_BIOS -ComputerName $computer
+                $cs = Get-CimInstance -ClassName Win32_ComputerSystem -CimSession $cim
+                $bios = Get-CimInstance -ClassName Win32_BIOS -CimSession $cim
     
                 # Get total physical memory
                 $totalram = "{0:N2}" -f ($cs.TotalPhysicalMemory / 1GB) + " GB"
@@ -156,6 +157,7 @@ Function Get-SystemData {
         
             } # end if-statement
         
+            Remove-CimSession $cim
         } # end foreach-loop $computer
     
     } # end PROCESS block
@@ -216,12 +218,14 @@ function Get-SystemVolumeData {
     
         foreach ($computer in $ComputerName) {
             Write-Verbose "Querying $computer"
+            $cim = New-CimSession -ComputerName $computer -Credential $admin
     
             # test for online systens
             try {
                 $passtest = $true
                 # extract only local disk data
-                $data = Get-CimInstance -ClassName Win32_Volume -ComputerName $computer -Filter "DriveType = 3" -ErrorAction Stop
+                $data = Get-CimInstance -ClassName Win32_Volume -CimSession $cim -Filter "DriveType = 3" -ErrorAction Stop |
+                    Where-Object {$_.Name -notlike '\\?*'}
             }
             catch {
                 if ($LogErrors) {
@@ -248,7 +252,7 @@ function Get-SystemVolumeData {
     
                     # Custom output object
                     $object = New-Object -TypeName psobject -Property $properties
-                    $object.psobject.TypeNames.Insert(1, 'LCO.SystemVolumeData')
+                    $object.psobject.TypeNames.Insert(1, 'LCO.VolumeData')
                     Write-Output $object
     
                 } # end foreach-loop $drive
@@ -258,6 +262,7 @@ function Get-SystemVolumeData {
     
             } # end if statement
     
+            Remove-CimSession $cim
         } # end foreach-loop $computer
     
     } # end PROCESS block
@@ -320,11 +325,12 @@ function Get-SystemServiceData {
     
         foreach ($computer in $ComputerName) {
             Write-Verbose "Querying $computer"
+            $cim = New-CimSession -ComputerName $computer -Credential $admin
     
             # Check for online systems
             try {
                 $everything_ok = $true
-                $services = Get-CimInstance -ClassName Win32_Service -ComputerName $computer -Filter "State = 'Running'" -ErrorAction Stop
+                $services = Get-CimInstance -ClassName Win32_Service -CimSession $cim -Filter "State = 'Running'" -ErrorAction Stop
             }
             catch {
                 if ($LogErrors) {
@@ -347,7 +353,7 @@ function Get-SystemServiceData {
                     }
         
                     # Get services corresponding process data
-                    $process = Get-CimInstance -ClassName Win32_Process -ComputerName $computer -Filter "ProcessID = '$($service.ProcessID)'"
+                    $process = Get-CimInstance -ClassName Win32_Process -CimSession $cim -Filter "ProcessID = '$($service.ProcessID)'"
                         
                     # adding process data to hash table
                     $properties.Add('ProcessName', $process.ProcessName)
@@ -365,6 +371,7 @@ function Get-SystemServiceData {
     
             } # end if statement
                 
+            Remove-CimSession $cim
         } # end foreach-loop $computer
     
     } # end PROCESS block
@@ -419,7 +426,7 @@ function Set-ServicePassword {
     
     PROCESS {
         foreach ($computer in $ComputerName) {
-            $svcs = Get-WmiObject -Class Win32_Service -ComputerName $computer -Filter "name='$ServiceName'"
+            $svcs = Get-CimInstance -ClassName Win32_Service -ComputerName $computer -Filter "name='$ServiceName'"
     
             foreach ($svc in $svcs) {
                 if ($PSCmdlet.ShouldProcess("$svc on $computer")) {
@@ -477,7 +484,7 @@ function Set-ComputerState {
 
         foreach ($computer in $ComputerName) {
             Write-Verbose "Processing $computer"
-            $os = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $computer
+            $os = Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName $computer
 
             if ($PSCmdlet.ShouldProcess($computer)) {
                 Write-Verbose "Passing flag $flag"
@@ -586,6 +593,100 @@ function Get-RemoteSmbShare {
 
 } # end function
 
+function Get-SystemSoftware {
+
+    <#
+    .SYNOPSIS
+    Script retrives software list from a system.
+    
+    .DESCRIPTION
+    Get-SystemSoftware uses Get-ItemProperty to query a system or group 
+    of systems registry and return a collection of software applications
+    that are installed on the system. 
+    
+    .PARAMETER ComputerName
+    One or more computer names or IP addresses to collect software from.
+    
+    .PARAMETER LogErrors
+    Specify this switch to create a text log file of computers that could
+    not be queried.
+    
+    .PARAMETER ErrorLog
+    When used with -LogErrors, specifies the file path and name to which 
+    failed computer names will be written. Defaults to C:\Retry.txt
+    
+    .EXAMPLE 
+    Get-Content names.txt | Get-SystemSoftware
+    
+    .EXAMPLE
+    Get-SystemSoftware -ComputerName SERVER1, SERVER2
+        
+    #>
+    
+    [CmdletBinding()]
+    param (
+        # Parameter help description
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [string[]]
+        $ComputerName,
+
+        [string]$ErrorLog = $ErrorLogPreference,
+
+        [switch]$LogErrors
+    )
+    
+    begin { Write-Verbose -Message "Starting software collection...." }
+
+    process {
+        foreach ($Computer in $ComputerName) {
+            Write-Verbose -Message "Querying $Computer"
+    
+            try {
+                $pass = $true
+                $apps = Invoke-Command -ScriptBlock {
+                    Get-ItemProperty -Path HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |
+                        Where-Object {$null -ne $_.DisplayName}
+                } -ComputerName $Computer -Credential $admin -ErrorAction Stop
+            }
+            catch {
+                if ($LogErrors) {
+                    Write-Warning -Message "$Computer did not respond."
+                    $Computer | Out-File -FilePath $ErrorLog -Append
+                    $pass = $false
+                    Write-Warning -Message "System name logged at $ErrorLog"
+                }
+            }
+    
+            # Computer online. Process data
+            if ($pass) {
+                # Iterate through each app
+                foreach ($app in $apps) {
+                    $props = @{
+                        ComputerName = $Computer
+                        DisplayName  = $app.DisplayName
+                        Version      = $app.DisplayVersion
+                        InstallDate  = $app.InstallDate
+                    }
+    
+                    # Custom output object
+                    $obj = New-Object -TypeName psobject -Property $props
+                    Write-Output -InputObject $obj
+    
+                    # Remove app variable for next computer
+                    Remove-Variable -Name app
+    
+                } # end foreach-loop $apps
+    
+            } # end if-statement
+    
+        } # end foreach-loop $Computer
+
+    } # end process block
+
+    end { Write-Verbose "Queries completed" }
+
+} # end function
+
 # Export preference variable and all functions
 Export-ModuleMember -Variable ErrorLogPreference
-Export-ModuleMember -Function Get-SystemData, Get-SystemVolumeData, Get-SystemServiceData, Get-ComputerNamesFromDatabase, Set-InventoryInDatabase, Set-ServicePassword, Set-ComputerState, Get-RemoteSmbShare
+Export-ModuleMember -Function *-*
