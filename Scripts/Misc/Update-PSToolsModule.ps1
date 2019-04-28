a4b6ef4666684bf10a4e50f1d7c393279c0e80da
Alvaro Lupercio <luper@lco-wl1.lco.net>
2019-04-28 14:22:45 -0400

    Updated path on Update-PSToolsModule script.

--- Scripts/Misc/Update-PSToolsModule.ps1
+++ Scripts/Misc/Update-PSToolsModule.ps1
@@ -1,6 +1,6 @@
 # Script used to copy the changes made to APLTools and re-importing the module with the updates
-Copy-Item -Force -Recurse -Path D:\GitHub\PSTools\Modules\PSTools -Destination 'C:\Program Files\WindowsPowerShell\Modules' -PassThru
-Copy-Item -Force -Recurse -Path D:\GitHub\PSTools\Modules\PSTools -Destination 'C:\Program Files\PowerShell\6-preview\Modules' -PassThru 
+Copy-Item -Force -Recurse -Path C:\Users\luper\GitRepo\PSTools\Modules\PSTools -Destination 'C:\Program Files\WindowsPowerShell\Modules' -PassThru
+Copy-Item -Force -Recurse -Path C:\Users\luper\GitRepo\PSTools\Modules\PSTools 'C:\Program Files\PowerShell\6\Modules' -PassThru 
 
 # Update module in powershel
 Remove-Module -Name PSTools -ErrorAction SilentlyContinue 
